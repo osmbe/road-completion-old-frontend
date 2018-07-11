71bab7a102c98a70435b0fbd33acb3954311f0de
Jodi De Loof <jodideloof@Jodis-MacBook-Pro.local>
2018-07-11 11:09:29 +0200

    added listening + mlab connection

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/issuedb');
+
+mongoose.connection.on("open", function(ref) {
+  console.log("Connected to mongodb server");
+  return start_up();
+});
+
+mongoose.connect('mongodb://osoctest:osoc123@ds139890.mlab.com:39890/osoc18road',{ useNewUrlParser: true });
+
 require('./models/Issue');
 
 var indexRouter = require('./routes/index');
@@ -13,23 +20,25 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+app.use('/API/', indexRouter);
+
+app.listen(3000,function(){
+  console.log("App running on port 3000");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -37,7 +46,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err.message);
 });
 
 module.exports = app;
