a335668705132eb4eb89fba5b023c1f787a02f8a
HispanicMojitos <andrewsleruth@gmail.com>
2018-07-13 09:48:27 +0200

    organized code a little bit

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var app = express();
+let cors = require('cors');
+require('./models/Issue');
 
 
-mongoose.connection.on("open", function(ref) {
+mongoose.connection.on("open", (ref) => {
   console.log("Connected to mongodb server");
 });
 
 //mongoose.connect('mongodb://osoctest:osoc123@ds139890.mlab.com:39890/osoc18road',{ useNewUrlParser: true });
 mongoose.connect('mongodb://localhost:27017/osoc18road',{ useNewUrlParser: true });
 
-require('./models/Issue');
-
-var indexRouter = require('./routes/index');
-
-var app = express();
-
-
-let cors = require('cors');
 app.use(cors({origin: "*"}));
 // view engine setup
 app.set('view engine', 'jade');
@@ -38,11 +34,11 @@ app.listen(3000,function(){
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
