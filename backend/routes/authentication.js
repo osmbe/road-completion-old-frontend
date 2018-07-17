e6c7ce9d81b39c252648c96d8b15e4d54ce05e31
HispanicMojitos <andrewsleruth@gmail.com>
2018-07-17 14:04:53 +0200

    Revert "Revert "Authentication works now""
    This reverts commit 82002b354a6add5e23fa416e4bcb29b878561a73.

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -8,41 +8,47 @@ passport.use(osmStrategy);
 
 // Apparently the ensureAuthenticated function needs to be created for 
 var ensureAuthenticated = (req,res,next) => {
-    if(req.isAuthenticated()) { return next(); }
+    /*if(req.isAuthenticated()) { return next(); }
     else
     {
         // Return error or redirect
-        res.redirect(('/AUTH/openstreetmap'));
+        res.redirect(('/AUTH/login'));
         //res.send(false);
-    }
+    }*/
+    req.isAuthenticated() ? next() : 
+        res.failureRedirect('/AUTH/login');
 }
 
 router.get('/', (req, res) => {
-    res.render('index', {user: req.user});
+    res.send({});
 });
 
 router.get('/account', ensureAuthenticated, (req, res) => {
-    res.send({user: req.user.displayName});
+    res.send({user: req.user});
 });
 
-router.get('/login', passport.authenticate('openstreetmap', {failureRedirect: '/login'}), (req, res) => {
-    res.render('login', {user: req.user.username});
+router.get('/login', (req, res) => {
+    res.redirect('/AUTH/openstreetmap/');
 });
 
 router.get('/openstreetmap', passport.authenticate('openstreetmap'),
 (req, res) => {});
 
-router.get('/isAuth',  (req, res) => {
-    res.json({ username: req.user.username} );
+router.get('/isAuth', (req, res) => {
+    res.json({ username: req.user} );
 });
 
 router.get('/openstreetmap/callback', passport.authenticate('openstreetmap', { failureRedirect: '/login'}), (req, res) => {
-    res.redirect('/AUTH/account');
+    //res.redirect('/AUTH/isAuth');
+    res.send({username: req.user});
 });
 
 router.get('/logout', (req, res) => {
     req.logout();
-    res.redirect('/');
+    req.session.destroy((err) => {
+        if (err) return next(err);
+        return res.send({authenticated: req.isAuthenticated()});
+    });
 });
 
 module.exports = router;
